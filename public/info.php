2cff0194f6d244e55c4e72f632ea2508ecbc9865
Miguel Ramos <miguel.marques.ramos@gmail.com>
2019-12-03 14:27:32 +0000

    Remove deprecated php option

--- public/info.php
+++ public/info.php
@@ -1,2 +1,20 @@
 <?php
+
+declare(strict_types=1);
+
+/*
+ +------------------------------------------------------------------------+
+ | NQDA Platform                                                          |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2019 NQDA Team                                           |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file LICENSE.txt.                             |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to license@nqda.pt so we can send you a copy immediately.              |
+ +------------------------------------------------------------------------+
+*/
+
 phpinfo();
